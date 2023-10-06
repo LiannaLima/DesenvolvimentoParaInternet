b551addde961dcf08fe1ab0cd2c2640fb7faae52
LiannaLima <77620290+LiannaLima@users.noreply.github.com>
2023-10-06 11:46:03 -0300

    Add files via upload

---
+++ backend1-main (1)/backend1-main/src/main/java/com/example/demo/controllers/CursoController.java
@@ -0,0 +1,43 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.Curso;
+import com.example.demo.services.CursoService;
+
+@RestController
+@RequestMapping("/curso")
+public class CursoController {
+
+    @Autowired
+    CursoService cursoService;
+    
+    @GetMapping("/{idCurso}")
+    public Curso getCurso(@PathVariable Integer idCurso) {
+        Curso curso = cursoService.getCurso(idCurso);
+        return curso;
+    }
+
+    @PostMapping
+    public void cadastrarCurso(@RequestBody Curso curso) {
+        cursoService.salvar(curso);
+    }
+
+    @PutMapping
+    public void atualizarCurso(@RequestBody Curso curso) {
+        cursoService.atualizar(curso);
+    }
+
+    @DeleteMapping("/{idCurso}")
+    public void removerCurso(@PathVariable Integer idCurso) {
+        cursoService.remover(idCurso);
+    }
+}
